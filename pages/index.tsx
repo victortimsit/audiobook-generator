7bf899714e349d2d82c0e2b52a3c0328f1390a75
victortimsit <victortimsit.contact@gmail.com>
2023-03-24 15:33:09 +0100

    update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,30 +130,7 @@ const Home: NextPage = () => {
                 Mrs Dursley was thin and blonde and had nearly twice the usual
                 amount of neck, which came in very useful as she spent so much
                 of her time craning over garden fences, spying on the
-                neighbours. The Dursleys had a small son called Dudley and in
-                their opinion there was no finer boy anywhere. The Dursleys had
-                everything they wanted, but they also had a secret, and their
-                greatest fear was that somebody would discover it. They didn’t
-                think they could bear it if anyone found out about the Potters.
-                Mrs Potter was Mrs Dursley’s sister, but they hadn’t met for
-                several years; in fact, Mrs Dursley pretended she didn’t have a
-                sister, because her sister and her good-for-nothing husband were
-                as unDursleyish as it was possible to be. The Dursleys shuddered
-                to think what the neighbours would say if the Potters arrived in
-                the street. The Dursleys knew that the Potters had a small son,
-                too, but they had never even seen him. This boy was another good
-                reason for keeping the Potters away; they didn’t want Dudley
-                mixing with a child like that. When Mr and Mrs Dursley woke up
-                on the dull, grey Tuesday our story starts, there was nothing
-                about the cloudy sky outside to suggest that strange and
-                mysterious things would soon be happening all over the country.
-                Mr Dursley hummed as he picked out his most boring tie for work
-                and Mrs Dursley gossiped away happily as she wrestled a
-                screaming Dudley into his high chair. None of them noticed a
-                large tawny owl flutter past the window. At half past eight, Mr
-                Dursley picked up his briefcase, pecked Mrs Dursley on the cheek
-                and tried to kiss Dudley goodbye but missed, because Dudley was
-                now having a tantrum and throwing his cereal at the walls.
+                neighbours...
               </div>
               <ArrowRightIcon className="hidden  md:block w-8 h-8 text-white/30 mt-36" />
               <video
